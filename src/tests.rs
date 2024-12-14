255eb4f975cf369473ed2105fc53e11a4d34247c
Krishna Nagam <krishna_nagam@Krishnas-MacBook-Air.local>
2024-12-14 20:39:56 +0530

    removed bad line

--- src/tests.rs
+++ src/tests.rs
@@ -25,6 +25,3 @@ mod tests {
         assert_eq!(result,"                %_20                %_20".to_string());
     }
 }
-
-
-{"adults":"2","airline":"","carrier":"","childs":"1","class":"Economy","depart_date":"07/11/2024","destination":"BOM - Mumbai, IN","flexi_search":"","from":"SIN","infants":"0","intl":"y","origin":"SIN - Singapore, SG","page":"","return_date":"","sd":"1726727794818","sellingCountry":"IN","sft":"","ssfc":"","ssfi":"","to":"BOM"}
